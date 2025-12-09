715330a3c824d8da253b7187f0b90e2896227050
Raul Pérez Hinojal <raul.perez@gracia.lasalle.cat>
2025-12-09 19:14:41 +0100

    creo las variablees globales y pongo las cordenadas iniciales

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+#variables globales
+# mode 0 = En espera
+# mode 1 = Estació Meteorològica (Gràfic Temperatura)
+# mode 2 = Moure la Gota (Acceleròmetre)
 
+mode_seleccionat = 0
+# Coordenades inicials de la gota (centre de la pantalla)
+gota_x = 2
+gota_y = 2
--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-
+// variables globales
+//  mode 0 = En espera
+//  mode 1 = Estació Meteorològica (Gràfic Temperatura)
+//  mode 2 = Moure la Gota (Acceleròmetre)
+let mode_seleccionat = 0
+//  Coordenades inicials de la gota (centre de la pantalla)
+let gota_x = 2
+let gota_y = 2
